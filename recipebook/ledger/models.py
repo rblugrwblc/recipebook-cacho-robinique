35fe9fb7805e01c2574fbc61bbff7794f51edcc5
cachooo <70294001+cachooo@users.noreply.github.com>
2025-03-07 19:10:49 +0800

    Feat: Ingredient, Recipe and RecipeIngredient classes to handle models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ingredient_detail', args=[str(self.id)])
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('recipe_detail', args=[str(self.id)])
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipe")
+    quantity = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.ingredient.name} in {self.recipe.name if self.recipe else 'Unknown Recipe'}"
